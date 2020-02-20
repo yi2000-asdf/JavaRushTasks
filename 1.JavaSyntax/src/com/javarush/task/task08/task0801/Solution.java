55fccb3da066bf06fe07d356f06086409c9b6f85
yi2000 <yi2000@protonmail.com>
2020-02-20 22:32:46 +0300

    #1

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task08.task0801;
+
+/* 
+Set из растений
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
