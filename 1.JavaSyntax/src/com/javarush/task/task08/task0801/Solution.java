ab8b5719f2ed8d718e593c9c3ce49588a3426def
yi2000 <yi2000@protonmail.com>
2020-02-22 19:24:09 +0300

    #1

--- 1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
@@ -5,12 +5,28 @@ Set из растений
 */
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        Set<String> s = new HashSet<String>();
+        s.add("арбуз");
+        s.add("банан");
+        s.add("вишня");
+        s.add("груша");
+        s.add("дыня");
+        s.add("ежевика");
+        s.add("женьшень");
+        s.add("земляника");
+        s.add("ирис");
+        s.add("картофель");
+
+        Iterator<String> i = s.iterator();
+        while (i.hasNext())
+
+            System.out.println(i.next());
 
 
     }
